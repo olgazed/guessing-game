4bad8ee4528d32b848cd04fc66e2071d4eeb66c7
Olga Belikova <lakshmy712@mail.ru>
2019-10-11 16:33:46 +0300

    feat: add guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.min = 0;
+    	this.max = 0;
+    	this.guessed = 0;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	this.guessed = Math.round((this.max + this.min) / 2);
+    	return this.guessed;
     }
 
     lower() {
-
+    	this.max = this.guessed;
+    	return this.max;
     }
 
     greater() {
-
+    	this.min = this.guessed;
+    	return this.min;
     }
 }
 
